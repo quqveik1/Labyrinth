6b847e0fd14441d1822497f0ce4ba8a1341846a0
Alex Bogomol <quqveik.1@gmail.com>
2023-02-08 00:01:01 +0300

    Initial commit

---
+++ app/src/main/java/com/kurlic/labirints/MainActivity.java
@@ -0,0 +1,15 @@
+package com.kurlic.labirints;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Window;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
